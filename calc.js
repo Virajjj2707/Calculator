0811c1d3ad233f6d65c850c2e23c181ac19b3cb6
Virajjj2707 <virajvadhane007@gmail.com>
2022-10-18 20:19:55 +0530

    Input Changes

--- calc.js
+++ calc.js
@@ -1,3 +1,24 @@
-console.log (
 
-);
+
+document.getElementById("answer").readOnly = true; //set this attribute in Html file
+let screen = document.getElementById("answer");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+    item.addEventListener("click", (e) => {
+        buttonText = e.target.innerText;
+        if (buttonText == "X") {
+            buttonText = "*";
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == "C") {
+            screenValue = "";
+            screen.value = screenValue;
+        } else if (buttonText == "=") {
+            checkForBracketMulti(); // automatically evaluates if no brackets
+        } else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    });
+}
