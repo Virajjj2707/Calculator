a7b090f12590ea0db879a55857fbb31f99f75004
Virajjj2707 <virajvadhane007@gmail.com>
2022-10-20 14:21:22 +0530

    Functions

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 
 
-document.getElementById("answer").readOnly = true; //set this attribute in Html file
+document.getElementById("answer").readOnly = true; //this attribute is set in HTML file
 let screen = document.getElementById("answer");
 buttons = document.querySelectorAll("button");
 let screenValue = "";
@@ -15,10 +15,24 @@ for (item of buttons) {
             screenValue = "";
             screen.value = screenValue;
         } else if (buttonText == "=") {
-            checkForBracketMulti(); // automatically evaluates if no brackets
+            checkForBracketMulti(); // automatically evaluates values if no brackets
         } else {
             screenValue += buttonText;
             screen.value = screenValue;
         }
     });
-}
+} 
+
+document.getElementById("keydown", function(event){
+            console.log(event.which);
+            if(event.shiftKey == 57){
+                event.key = "(";
+            } else if (event.shiftKey == 48){
+                event.key = ")";
+            } else if(event.shiftKey == 53){
+                event.key = "%"
+            } if (event.keyCode == 88){
+                event.key += "*"
+                screen.value = screenValue
+            } 
+})
